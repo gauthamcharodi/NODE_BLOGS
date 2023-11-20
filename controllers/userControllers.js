c457998e406024bec9663c136d3348ce2d96c046
gauthamcharodi <gauthamcharodi23@gmail.com>
2023-11-20 18:15:42 +0530

    changes done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,12 @@
 const User = require("../models/User");
+const jwt=require("jsonwebtoken")
+
+const genToken=async(id)=>{
+  return await jwt.sign({id:id},process.env.JWT_SECRET,{
+    expiresIn:24*60*60
+  })
+}
+
 const signup = async (req, res) => {
   const { name, email, password, confirmPassword } = req.body;
   try {
@@ -10,9 +18,12 @@ const signup = async (req, res) => {
         message: "user exists already,try logging in",
       });
     }
+
     const newUser = await User.create(req.body);
+    const token =await genToken(newUser._id)
     res.status(201).json({
       status: "success",
+      token,
       data: {
         newUser,
       },
@@ -27,18 +38,24 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const existingUser = await User.findOne({ email: req.body.email });
-    const isMatch=await existingUser.comparePassword( req.body.
-      password,existingUser.password) 
-    if (!existingUser && !isMatch){
+    if(!req.body.email || !req.body.password){
+      return res.status(400).json({
+        status: "fail",
+        message: "please enter credentials",
+      });
+    }
+    const existingUser = await User.findOne({ email: req.body.email }); 
+    if (!existingUser || !(await existingUser.comparePassword( req.body.
+      password,existingUser.password))){
       return res.status(400).json({
         status: "fail",
         message: "User name and password is not correct",
       });
     }
-
+    const token=await genToken(existingUser._id)
     res.status(200).json({
       status: "success",
+      token,
       data: {
         existingUser,
       },
