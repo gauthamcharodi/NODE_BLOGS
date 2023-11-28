459de68c092f7ea7ce5403c439eb0d4a3dc1904d
gauthamcharodi <gauthamcharodi23@gmail.com>
2023-11-28 18:18:48 +0530

    custom

--- controllers/blogcontroller.js
+++ controllers/blogcontroller.js
@@ -69,17 +69,19 @@ const getBlogs = async (req, res) => {
     let search = req.query.search || "";
     let page = req.query.page * 1 || 1;
     let limit = req.query.limit * 1 || 3;
-    let sort=req.query.sort || "rating"
+    let sort = req.query.sort || "rating";
     let skip = (page - 1) * limit;
-  //ratings,year //ratings year
-    sort && sort.split(",").join(" ")
+    //ratings,year //ratings year
+    sort && sort.split(",").join(" ");
 
     const blogs = await blogModel
       .find({ title: { $regex: search, $options: "i" } })
-      .skip(skip) .limit(limit).sort(sort);
-      
-      let totalBlogs=await blogModel.countDocuments()
-      res.status(200).json({
+      .skip(skip)
+      .limit(limit)
+      .sort(sort);
+
+    let totalBlogs = await blogModel.countDocuments();
+    res.status(200).json({
       status: "success",
       page,
       limit,
@@ -96,38 +98,50 @@ const getBlogs = async (req, res) => {
   }
 };
 
-const updateBlog=async(req,res)=>{
+const updateBlog = async (req, res) => {
   try {
-      const {id}=req.params
-      const {title,description,snippet,image,ratings}=req.body
-      if(req.user.role==='author'){
-          const updatedBlog=await blogModel.findByIdAndUpdate({_id:id},{$set:{title:title,snippet:snippet,description:description,image:image,}},{new:true,runValidators:true})
+    const { id } = req.params;
+    const { title, description, snippet, image, ratings } = req.body;
+    if (req.user.role === "author") {
+      const updatedBlog = await blogModel.findByIdAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            title: title,
+            snippet: snippet,
+            description: description,
+            image: image,
+          },
+        },
+        { new: true, runValidators: true }
+      );
       res.status(200).json({
-          status:'success',
-          data:{
-              updatedBlog
-          }
-      })
-      }
-      if(req.user.role==='user'){
-          const updatedBlog=await .findByIdAndUpdate({_id:id},{$set:{ratings:ratings}},{new:true,runValidators:true})
+        status: "success",
+        data: {
+          updatedBlog,
+        },
+      });
+    }
+    if (req.user.role === "user") {
+      const updatedBlog = await blogModel.findByIdAndUpdate(
+        { _id: id },
+        { $set: { ratings: ratings } },
+        { new: true, runValidators: true }
+      );
       res.status(200).json({
-          status:'success',
-          data:{
-              updatedBlog
-          }
-      })
-      }
-
+        status: "success",
+        data: {
+          updatedBlog,
+        },
+      });
+    }
   } catch (error) {
-      res.status(400).json({
-          status:'fail',
-          message:error.message
-      })
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
   }
-}
-
-
+};
 
 const deleteBlog = async (req, res) => {
   try {
